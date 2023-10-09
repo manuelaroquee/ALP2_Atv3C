a1035c6a4e893788812e864eb39a54fd28b7d5c9
Manuela Roque <65978895+manuelaroquee@users.noreply.github.com>
2023-10-09 17:06:54 -0400

    Create Alg1BoaNoite.c

---
+++ Alg1BoaNoite.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+   int cont=1; 
+   
+    while(cont<=1000) {
+        printf("%d.Boa Noite, ", cont);
+        cont = cont + 1;
+    }
+
+    return 0;
+   }
+}
